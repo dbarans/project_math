6b6a74259908ec1ed73ba2bb357b9d7108a78899
Dominik Baranski <baranskidominik109@gmail.com>
2022-04-24 12:35:53 +0200

    implementacja funkcji rownania kwadratowego

--- mathematics/algebra.py
+++ mathematics/algebra.py
@@ -16,3 +16,19 @@ def div(a: int, b: int):
 
 def mul(a: int, b: int):
     return a * b
+
+def rownanie_kwadr(a: int, b: int, c: int):
+    delta = b**2 - 4*a*c
+    x1 = (-b + delta**(1/2))/2*a
+    x2 = (-b - delta**(1/2))/2*a
+    if x1 == x2:
+        return x1
+    elif type(x1) == complex:
+        return "brak rozwiązań"
+
+    else:
+        return x1, x2
+
+print(rownanie_kwadr(1,-1,0))
+print(rownanie_kwadr(1,6,9))
+print(rownanie_kwadr(3,0,4))
