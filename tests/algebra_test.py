8ad2d7597e696da2f8cb6eec3cc7e80c1e070a7e
Dominik Baranski <baranskidominik109@gmail.com>
2022-04-24 10:34:55 +0200

    testy dla funkcji sub

--- tests/algebra_test.py
+++ tests/algebra_test.py
@@ -4,3 +4,7 @@ from mathematics import algebra
 class TestAdd:
     def test_add(self):
         assert algebra.add(1, 4) == 5
+
+class TestSub:
+    def test_add(self):
+        assert algebra.sub(1, 4) == -3
