452812575d8a1372c33f86f3f84a59ca9d2a6842
Dominik Baranski <baranskidominik109@gmail.com>
2022-04-24 11:15:59 +0200

    implementacja funkcji  kolo

--- mathematics/geometry.py
+++ mathematics/geometry.py
@@ -1,3 +1,4 @@
+import math
 def kwadrat(a):
     return a ** 2
 
@@ -6,3 +7,10 @@ def prostokat(a, b):
 
 def trojkat(a, b):
     return a * b / 2
+
+def kolo(r):
+    return math.pi * (r ** 2)
+
+
+
+3.141592653589793
