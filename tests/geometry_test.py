78d70046894e1061d500d4eab6dcbcb572f5f8b8
Dominik Baranski <baranskidominik109@gmail.com>
2022-04-24 10:52:25 +0200

    test funkcji prostokat

--- tests/geometry_test.py
+++ tests/geometry_test.py
@@ -1,5 +1,9 @@
 from mathematics import geometry
 
 class TestKwadrat:
-    def test_Kwadrat(self):
+    def test_kwadrat(self):
         assert geometry.kwadrat(5) == 25
+
+class TestProstokat:
+    def test_prostokat(self):
+        assert geometry.prostokat(5, 6) == 30
